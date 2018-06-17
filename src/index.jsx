68c4066d423a19b5ace336bbf91433d1035f10ac
gergana <gergana_ruse@yahoo.com>
2018-06-17 10:46:44 +0100

    initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/app';
+//import { location, forecasts } from './data/forecast.json';
 
-render(<h1>Hello World!</h1>, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
+//location={location} forecasts={forecasts}
